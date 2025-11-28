d5883826e5e0245d0a0e9854949d8eb4986080a9
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:31:27 +0530

    Add files via upload

---
+++ program_32.cpp
@@ -0,0 +1,12 @@
+// program32.cpp
+#include <iostream>
+using namespace std;
+int main() {
+    try {
+        int arr[3] = {1, 2, 3};
+        cout << "Array element: " << arr[0] << endl;
+    } catch (...) {
+        cout << "Exception caught" << endl;
+    }
+    return 0;
+}
