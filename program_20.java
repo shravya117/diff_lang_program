d14364978ec03c974ca3aab778328889e30cbdbb
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:28:38 +0530

    Add files via upload

---
+++ program_20.java
@@ -0,0 +1,8 @@
+// program20.java
+public class StringDemo {
+    public static void main(String[] args) {
+        String text = "Java Programming";
+        System.out.println("Length: " + text.length());
+        System.out.println("Uppercase: " + text.toUpperCase());
+    }
+}
