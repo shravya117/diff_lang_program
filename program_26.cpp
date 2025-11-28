19a8848d53c5a7d711856dcdef50398fbff965a5
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:29:56 +0530

    Add files via upload

---
+++ program_26.cpp
@@ -0,0 +1,10 @@
+// program26.cpp
+#include <iostream>
+using namespace std;
+int add(int a, int b) {
+    return a + b;
+}
+int main() {
+    cout << "Sum: " << add(5, 3) << endl;
+    return 0;
+}
