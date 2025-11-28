4aba8bf9c4bf1310b66161e2e3a8cd6cf6262c25
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:31:13 +0530

    Add files via upload

---
+++ program_31.cpp
@@ -0,0 +1,14 @@
+// program31.cpp
+#include <iostream>
+using namespace std;
+class Calculator {
+public:
+    int multiply(int a, int b) {
+        return a * b;
+    }
+};
+int main() {
+    Calculator calc;
+    cout << "Result: " << calc.multiply(4, 5) << endl;
+    return 0;
+}
