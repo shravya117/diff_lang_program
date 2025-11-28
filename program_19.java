811ebfdffb153a6fa786254f914cbae0763d69af
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:28:25 +0530

    Add files via upload

---
+++ program_19.java
@@ -0,0 +1,9 @@
+// program19.java
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] arr = {5, 10, 15, 20};
+        for (int num : arr) {
+            System.out.println(num);
+        }
+    }
+}
