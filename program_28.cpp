46b81ff6f746683ed03927b62734ba62c64ea12f
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:30:26 +0530

    Add files via upload

---
+++ program_28.cpp
@@ -0,0 +1,10 @@
+// program28.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string text = "C++ String";
+    cout << "Length: " << text.length() << endl;
+    cout << "Text: " << text << endl;
+    return 0;
+}
