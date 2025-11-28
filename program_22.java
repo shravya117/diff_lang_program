b9c49674efcd126e818606b09e42df402904b3ad
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:29:01 +0530

    Add files via upload

---
+++ program_22.java
@@ -0,0 +1,11 @@
+// program22.java
+public class Conditional {
+    public static void main(String[] args) {
+        int score = 85;
+        if (score >= 80) {
+            System.out.println("Grade: A");
+        } else {
+            System.out.println("Grade: B");
+        }
+    }
+}
