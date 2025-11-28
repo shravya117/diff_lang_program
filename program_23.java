e94a5130151b49cd9e9c127380ac82893db14179
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:29:16 +0530

    Add files via upload

---
+++ program_23.java
@@ -0,0 +1,7 @@
+// program23.java
+public class Lambda {
+    public static void main(String[] args) {
+        java.util.List<Integer> list = java.util.Arrays.asList(1, 2, 3, 4, 5);
+        list.forEach(n -> System.out.println(n * 2));
+    }
+}
