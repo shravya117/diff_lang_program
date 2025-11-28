e1065c7070e3de7691115439a7b034398d86dba2
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:30:57 +0530

    Add files via upload

---
+++ program_30.cpp
@@ -0,0 +1,12 @@
+// program30.cpp
+#include <iostream>
+using namespace std;
+int main() {
+    int age = 25;
+    if (age >= 18) {
+        cout << "You are an adult" << endl;
+    } else {
+        cout << "You are a minor" << endl;
+    }
+    return 0;
+}
