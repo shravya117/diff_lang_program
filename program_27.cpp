f4b95c688188de95bcd1b162268250ff4ca0f79e
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:30:16 +0530

    Add files via upload

---
+++ program_27.cpp
@@ -0,0 +1,12 @@
+// program27.cpp
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> v = {1, 2, 3, 4, 5};
+    for (int num : v) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
