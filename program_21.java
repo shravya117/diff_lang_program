eeff04c847056e9b228b67ae9001d5d66f4fa0cb
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:28:49 +0530

    Add files via upload

---
+++ program_21.java
@@ -0,0 +1,8 @@
+// program21.java
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
