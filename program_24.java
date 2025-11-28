8d97049004b76f3af620df0dd8008c3fcabf5837
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:29:32 +0530

    Add files via upload

---
+++ program_24.java
@@ -0,0 +1,11 @@
+// program24.java
+public class Exception {
+    public static void main(String[] args) {
+        try {
+            int result = 10 / 2;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error occurred");
+        }
+    }
+}
