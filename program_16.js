9efd4915ec0125be4915808ce86e45e4f3145dac
Shravya K <shravyarao117@gmail.com>
2025-11-28 20:27:47 +0530

    Add files via upload

---
+++ program_16.js
@@ -0,0 +1,6 @@
+// program16.js - Async function
+async function fetchData() {
+    console.log('Fetching data...');
+    return 'Data received!';
+}
+fetchData().then(result => console.log(result));
